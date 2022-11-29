4ecaca515bd80cd8db1cbf1a87e234ee54139561
Adruin02 <quinteroadruin68@gmail.com>
2022-11-29 10:39:50 -0500

    Error corregido

--- Negocio/L_Producto.cs
+++ Negocio/L_Producto.cs
@@ -20,7 +20,7 @@ namespace Negocio
         /// <returns>Retorna una cadena con nuevos datos</returns>
         public bool Insertar(Producto producto)
         {
-            return datosPorducto.Insertar(producto);
+            return datosProducto.Insertar(producto);
         }
         /// <summary>
         /// Llama al metodo de CargarDatos en producto
@@ -29,7 +29,7 @@ namespace Negocio
         /// <returns>Retorna datos de la base de datos</returns>
         public bool CargarDatos(DataGridView grilla)
         {
-            return datosPorducto.CargarDatos(grilla);
+            return datosProducto.CargarDatos(grilla);
         }
         /// <summary>
         /// LLama al metodo eliminar en producto
@@ -38,7 +38,7 @@ namespace Negocio
         /// <returns>elimina una cadena de datos seleccionado</returns>
         public bool Eliminar(int idProducto)
         {
-            return datosPorducto.Eliminar(idProducto);
+            return datosProducto.Eliminar(idProducto);
         }
         /// <summary>
         /// llama al metodo actualizar de producto
@@ -47,7 +47,7 @@ namespace Negocio
         /// <returns>Modifica una cadena de la base de datos</returns>
         public bool Actualizar(Producto producto)
         {
-            return datosPorducto.Actualizar(producto);
+            return datosProducto.Actualizar(producto);
         }
     }
 }
